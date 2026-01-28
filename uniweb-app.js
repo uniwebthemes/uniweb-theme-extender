d91e4ca14ffde4bc79221c1ba436c67d5bf19d2f
uniwebcommerce <uniwebcommerce@gmail.com>
2026-01-28 17:30:44 +0530

    added metafield

--- uniweb-app.js
+++ uniweb-app.js
@@ -1,48 +1,66 @@
-/*document.getElementById("open-picker").addEventListener("click", async () => {
+(() => {
+  /*document.getElementById("open-picker").addEventListener("click", async () => {
   const selected = await shopify.resourcePicker({ type: "product" });
   console.log(selected);
 });*/
-
-document
-  .getElementById("product--picker")
-  .addEventListener("click", async () => {
-    const res = await fetch("shopify:admin/api/2025-04/graphql.json", {
+  async function checkInstagramAccessTokenMetafield() {
+    const res = await fetch("shopify:admin/api/2026-01/graphql.json", {
       method: "POST",
       body: JSON.stringify({
         query: `
-      query GetProduct($id: ID!) {
+      query metafieldDefinition($id: ID!) {
         product(id: $id) {
-          title
+          id
         }
       }
     `,
         variables: { id: "gid://shopify/Product/8966805422326" },
       }),
     });
-
     const { data } = await res.json();
-    document.getElementById("product-title").innerText = data.product.title;
-  });
+    return data.product.title;
+  }
+  document
+    .getElementById("product--picker")
+    .addEventListener("click", async () => {
+      const res = await fetch("shopify:admin/api/2026-01/graphql.json", {
+        method: "POST",
+        body: JSON.stringify({
+          query: `
+      query GetProduct($id: ID!) {
+        product(id: $id) {
+          title
+        }
+      }
+    `,
+          variables: { id: "gid://shopify/Product/8966805422326" },
+        }),
+      });
 
-const ctx = document.getElementById("myChart");
+      const { data } = await res.json();
+      document.getElementById("product-title").innerText = data.product.title;
+    });
 
-new Chart(ctx, {
-  type: "bar",
-  data: {
-    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-    datasets: [
-      {
-        label: "# of Votes",
-        data: [12, 19, 3, 5, 2, 3],
-        borderWidth: 1,
-      },
-    ],
-  },
-  options: {
-    scales: {
-      y: {
-        beginAtZero: true,
+  const ctx = document.getElementById("myChart");
+
+  new Chart(ctx, {
+    type: "bar",
+    data: {
+      labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+      datasets: [
+        {
+          label: "# of Votes",
+          data: [12, 19, 3, 5, 2, 3],
+          borderWidth: 1,
+        },
+      ],
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
       },
     },
-  },
-});
+  });
+})();
